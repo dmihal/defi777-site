607cca1756ede297ea1a61cf80187e8b2fcc654d
David Mihal <david@dmihal.com>
2020-05-25 00:24:47 -0300

    Updates

---
+++ precache-manifest.3aadf107d30cf5875118252e05597ea7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4074437328d21a7684bb5ce9df2975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3c95aaf13ab1a1711b",
+    "url": "/static/css/main.9845b2c1.chunk.css"
+  },
+  {
+    "revision": "9653f24dc363a10fcaf4",
+    "url": "/static/js/2.7020055e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7020055e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3c95aaf13ab1a1711b",
+    "url": "/static/js/main.576a6104.chunk.js"
+  },
+  {
+    "revision": "3f8b7f4c04e766f3b98e",
+    "url": "/static/js/runtime-main.3486abd2.js"
+  },
+  {
+    "revision": "0915fe5a7c6361a94031fb038118c7fa",
+    "url": "/static/media/aave.0915fe5a.svg"
+  },
+  {
+    "revision": "c6c56ba8f047db33ed371869742f69c9",
+    "url": "/static/media/balancer.c6c56ba8.svg"
+  },
+  {
+    "revision": "04575a6d5f7ffc844850f4338a8989d8",
+    "url": "/static/media/dai.04575a6d.svg"
+  },
+  {
+    "revision": "211e45ddaf261406cbd8f7faea3b489e",
+    "url": "/static/media/eth.211e45dd.svg"
+  },
+  {
+    "revision": "bc7e0bde7dfb9adb29fec0a2adabfacb",
+    "url": "/static/media/github.bc7e0bde.svg"
+  },
+  {
+    "revision": "4b02bc11930246d53d859deaa960862f",
+    "url": "/static/media/link.4b02bc11.svg"
+  },
+  {
+    "revision": "c86093d2d6f035c5cf4cc6e0c7fc1188",
+    "url": "/static/media/linkethrsi.c86093d2.svg"
+  },
+  {
+    "revision": "216efb8dd6a1b0d686f211a6116ace00",
+    "url": "/static/media/mkr.216efb8d.svg"
+  },
+  {
+    "revision": "189bf36386d0fc11daea597b14f4c373",
+    "url": "/static/media/set.189bf363.svg"
+  },
+  {
+    "revision": "25f74878f4eb216c8843be461373251d",
+    "url": "/static/media/trip-7.25f74878.svg"
+  },
+  {
+    "revision": "c3eb8f56dc611b6fd573a6ec1b88f162",
+    "url": "/static/media/uniswap.c3eb8f56.svg"
+  },
+  {
+    "revision": "e77df2b1cda0fc7333b5c13ee78b9dce",
+    "url": "/static/media/usdc.e77df2b1.svg"
+  },
+  {
+    "revision": "c5710a846cee9c9c1849ecf542cb4fa0",
+    "url": "/static/media/wbtc.c5710a84.svg"
+  }
+]);
